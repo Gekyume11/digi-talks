d671cf30b3408ba8d46bc87774f0da4743aa2cb8
Gekyume11 <146461225+Gekyume11@users.noreply.github.com>
2025-01-28 20:04:51 +0530

    Hero

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,9 +1,91 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Hero = () => {
+
+  const images = [
+    {
+      path: "apple.png",
+      host: "gekyume11.github.io",
+      top: '15%',
+      left: '50%',
+    },
+    {
+      path: "saturn.png",
+      host: "gekyume11.github.io",
+      top: '24%',
+      left: '5%',
+    },
+    {
+      path: "bulb.png",
+      host: "gekyume11.github.io",
+      top: '20%',
+      left: '20%',
+    },
+    {
+      path: "triangle.png",
+      host: "gekyume11.github.io",
+      top: '70%',
+      left: '40%',
+    },
+  ];
+
+  const heroText = [
+    "A IT Based-Solutions company",
+    "Web Development Agency",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur sed fugit ad eligendi deleniti. Aspernatur aliquam ipsam at, dolorem debitis facere libero temporibus doloremque veniam odio voluptate, perferendis eveniet reprehenderit.",
+  ]
+
+  const buttonText = [
+    "Get Started",
+    "Contact Us",
+  ]
+
+
   return (
     <>
-      <section className={`h-dvh bg-center bg-cover bg-[linear-gradient(90deg,#000,#0f1c2d)] relative before:top-1/2 before:-translate-y-1/2 before:left-[40%] before:-translate-x-1/2 before:absolute before:w-1/2 before:rounded-full before:h-[120%] before:bg-[radial-gradient(circle,rgba(255,255,0,0.15),rgba(255,255,0,0))] before:blur-[480px] `}>
+      <section className={`h-dvh bg-center bg-cover relative before:top-1/2 before:-translate-y-1/2 before:left-[40%] before:-translate-x-1/2 before:absolute before:w-1/2 before:rounded-full before:h-[120%] grid grid-cols-2`}>
+
+        <div className='grid place-items-center'>
+          <div className='w-full grid gap-4'>
+
+            <span className='uppercase font-semibold tracking-[3px] text-sm sm:text-base text-red-800'>{heroText[0]}</span>
+
+            <h1 className='text-[clamp(1.875rem,1.3922rem_+_2.7586vw,3.875rem)] font-bold leading-[1.2] text-[rgba(0,0,0,.8)] uppercase'>{heroText[1]}</h1>
+
+            <p className='max-w-[620px] leading-[1.5] text-[rgba(0,0,0,.7)]'>{heroText[2]}</p>
+
+            <div className='flex'>
+
+              <a className='py-2 cursor-pointer rounded-md px-4 block tracking-[1px] transition-all duration-300 hover:tracking-[3px] text-white bg-red-900'>{buttonText[0]}</a>
+
+            </div>
+
+          </div>
+        </div>
+
+        <div className='grid place-items-center'>
+            <div className=' bg-[rgba(0,0,0,.1)] h-[75%] w-full grid place-items-center border-2 rounded-3xl border-dashed border-black'>
+              <p className='text-2xl'>Image</p>
+            </div>
+        </div>
+
+
+        {
+          images.map(({ path, top, left }, i) => {
+            return (
+              <Image src={'/assets/' + path} alt={'image'} width={(i !== 1 && i !== 3) ? 60 : 120} height={(i !== 1 && i !== 3) ? 60 : 120}
+                key={i}
+                className={`absolute -z-[1] ${i === 1 ? 'animate-[10s_spin_linear_infinite]' : i === 2 ? 'animate-[10s_bounce_linear_infinite]' : i === 0 ? 'animate-[6s_sideWave_linear_infinite]' : 'animate-[10s_sideWave_linear_infinite]'}`}
+                style={{
+                  top: `${top}`,
+                  left: `${left}`,
+                }}
+              />
+            )
+          })
+        }
+
       </section>
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,31 +2,33 @@ import React, { useEffect } from 'react'
 import Lenis from "lenis";
 import Header from '@/Components/Header';
 import Hero from '@/Components/Hero';
+import { AnimatePresence } from 'framer-motion';
 
 const index = () => {
 
   useEffect(() => {
-      if (typeof window !== "undefined") {
-        const lenis: Lenis = new Lenis({
-          duration: 1.4,
-          easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)) // easing function
-        });
-  
-        function raf(time: number): void {
-          lenis.raf(time);
-          requestAnimationFrame(raf);
-        }
-  
+    if (typeof window !== "undefined") {
+      const lenis: Lenis = new Lenis({
+        duration: 1.4,
+        easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)) // easing function
+      });
+
+      function raf(time: number): void {
+        lenis.raf(time);
         requestAnimationFrame(raf);
       }
+
+      requestAnimationFrame(raf);
+    }
   }, [])
 
   return (
     <>
-    
-      <Header />
 
-      <Hero />
+      <AnimatePresence>
+        <Header />
+        <Hero />
+      </AnimatePresence>
 
     </>
   )
